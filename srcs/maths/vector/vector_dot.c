257ed0a70f66904f2938d737d3fe74a24b778748
dcdnce <pforesti@student.42nice.fr>
2022-11-02 10:26:45 +0100

    🏷️  Norminette srcs/maths/

--- srcs/maths/vector/vector_dot.c
+++ srcs/maths/vector/vector_dot.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector_dot.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
+/*   By: pforesti <pforesti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 15:44:42 by mrattez           #+#    #+#             */
-/*   Updated: 2022/10/26 09:11:57 by mrattez          ###   ########.fr       */
+/*   Updated: 2022/11/02 10:25:38 by pforesti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ double	vec2_dot(t_vec2 a, t_vec2 b)
 
 double	vec2_dot2(t_vec2 a)
 {
-	return vec2_dot(a, a);
+	return (vec2_dot(a, a));
 }
 
 /* a • b */
@@ -31,5 +31,5 @@ double	vec3_dot(t_vec3 a, t_vec3 b)
 
 double	vec3_dot2(t_vec3 a)
 {
-	return vec3_dot(a, a);
+	return (vec3_dot(a, a));
 }
--- srcs/maths/vector/vector_matrix.c
+++ srcs/maths/vector/vector_matrix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector_matrix.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
+/*   By: pforesti <pforesti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 14:14:42 by mrattez           #+#    #+#             */
-/*   Updated: 2022/10/04 15:29:18 by mrattez          ###   ########.fr       */
+/*   Updated: 2022/11/02 10:26:08 by pforesti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,17 @@
 t_vec3	vec3_mat4(t_vec3 vec, t_mat4 mat)
 {
 	return ((t_vec3){
-		vec.x * mat.m[0][0] + vec.y * mat.m[0][1] + vec.z * mat.m[0][2] + mat.m[0][3],
-		vec.x * mat.m[1][0] + vec.y * mat.m[1][1] + vec.z * mat.m[1][2] + mat.m[1][3],
-		vec.x * mat.m[2][0] + vec.y * mat.m[2][1] + vec.z * mat.m[2][2] + mat.m[2][3]
+		vec.x * mat.m[0][0] \
+			+ vec.y * mat.m[0][1] \
+				+ vec.z * mat.m[0][2] \
+					+ mat.m[0][3],
+		vec.x * mat.m[1][0] \
+			+ vec.y * mat.m[1][1] \
+				+ vec.z * mat.m[1][2] \
+					+ mat.m[1][3],
+		vec.x * mat.m[2][0] \
+			+ vec.y * mat.m[2][1] \
+				+ vec.z * mat.m[2][2] \
+					+ mat.m[2][3]
 	});
 }
